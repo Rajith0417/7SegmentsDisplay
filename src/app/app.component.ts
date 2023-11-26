2f0126b60ca64c74d4c853a117666529e8b1132c
Raji G <rajith0417@gmail.com>
2023-11-26 13:33:48 +0530

    feat: dynamic input added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,14 +3,18 @@ import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { RegularNumberComponent } from './regular-number/regular-number.component';
 import { SevenSegmentNumberComponent } from './seven-segment-number/seven-segment-number.component';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, RegularNumberComponent, SevenSegmentNumberComponent],
+  imports: [CommonModule, RouterOutlet, RegularNumberComponent, SevenSegmentNumberComponent, ReactiveFormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = '7SegmentsDisplay';
+
+  typedValue = new FormControl<number>(0);
+
 }
